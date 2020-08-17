31044941af064aa812176fcc88908d36c1a5b242
Melissa <66856935+mlam0527@users.noreply.github.com>
2020-08-17 12:10:43 -0400

    added mapbox

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-function greeting(){
-  console.log("Hello World")
-}
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWxhbTA1MjciLCJhIjoiY2tkeXBpd3hmM2YwZDJ5bWhrcGs3aG50cyJ9.qggxLKYkmcZFUCx6tJqiHw';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
